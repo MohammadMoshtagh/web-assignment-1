e1a1f8a43e4f417f8e3d4cad2dae1414b5386a79
Mohammad <mhmmoshtaghi@gmail.com>
2024-03-07 01:25:53 +0330

    Change API names

--- src/main/java/edu/sharif/web/controller/CalculatorController.java
+++ src/main/java/edu/sharif/web/controller/CalculatorController.java
@@ -7,28 +7,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
-    @GetMapping("/add")
-    public int add(@RequestParam("x") int x, @RequestParam("y") int y) {
-        return x + y;
+    @GetMapping("/sum")
+    public String add(@RequestParam("x") int x, @RequestParam("y") int y) {
+        return String.format("%d + %d = %d", x, y, x + y);
     }
 
-    @GetMapping("/multiply")
-    public int multiply(@RequestParam("x") int x, @RequestParam("y") int y) {
-        return x * y;
+    @GetMapping("/mult")
+    public String multiply(@RequestParam("x") int x, @RequestParam("y") int y) {
+        return String.format("%d * %d = %d", x, y, x * y);
     }
 
     @GetMapping("/subtract")
-    public int subtract(@RequestParam("x") int x, @RequestParam("y") int y) {
-        return x - y;
+    public String subtract(@RequestParam("x") int x, @RequestParam("y") int y) {
+        return String.format("%d - %d = %d", x, y, x - y);
     }
 
-    @GetMapping("/divide")
-    public float divide(@RequestParam("x") int x, @RequestParam("y") int y) {
+    @GetMapping("/div")
+    public String divide(@RequestParam("x") int x, @RequestParam("y") int y) {
         if (y == 0) {
             throw new ArithmeticException("Division by zero!");
         }
 
-        return (float) x / y;
+        return String.format("%d / %d = %.2f", x, y, (float) x / y);
     }
 
 }
